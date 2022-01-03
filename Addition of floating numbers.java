77c2808fe128efc83e7f2b1ef1508f05f98abda2
ranjit2401 <88999174+ranjit2401@users.noreply.github.com>
2022-01-03 13:31:02 +0530

    Create Addition of floating numbers.java

---
+++ Addition of floating numbers.java
@@ -0,0 +1,14 @@
+class addfloat
+{
+  float a = 45.f;
+  float b = 77.f;
+}
+public class floataddition extends addfloat
+{
+  float add = a+b;
+  public static void main(String args[])
+  {
+    floataddition 0 = new floataddition();
+    System.out.println("Sum of float : "+o.add);
+  }
+}  
