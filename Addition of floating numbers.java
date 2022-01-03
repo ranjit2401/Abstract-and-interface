517d55b94541178cf1dd992314cc540d2cd41db0
ranjit2401 <88999174+ranjit2401@users.noreply.github.com>
2022-01-03 13:19:47 +0530

    Create Addition of floating numbers.java

---
+++ Addition of floating numbers.java
@@ -0,0 +1,18 @@
+interface addFloat
+{
+    public void add();
+}
+public class addusinginterface implements addFloat
+{
+    public void add()
+    {
+        float a = 20.0f;
+        float b = 77;
+        System.out.println(a+b);
+    }
+    public static void main(String args[]}
+    {
+        addusinginterface o = new  adddusinginnterface();
+        o.add();
+    }
+}    
